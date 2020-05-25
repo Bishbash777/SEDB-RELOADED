70a386fb8598788f6ef9e749028b1f8d6f42ff68
Jack Bishop <jack.bishop.pcs@gmail.com>
2020-05-26 00:15:00 +0100

    First pass at integrating Steamid and discord account linking

--- SEDiscordBridge/Commands.cs
+++ SEDiscordBridge/Commands.cs
@@ -1,8 +1,12 @@
 using NLog;
+using System.Net.Http;
 using Torch.Commands;
 using Torch.Commands.Permissions;
 using VRage.Game.ModAPI;
-
+using System.Collections.Generic;
+using System.Web;
+using Sandbox.Game;
+using System.Diagnostics;
 namespace SEDiscordBridge
 {
 
@@ -37,6 +41,30 @@ namespace SEDiscordBridge
                 Plugin.UnloadSEDB();
             }
             Context.Respond("SEDB plugin reloaded!");
+        }
+
+        [Command("sedb-link", "Link you steamID to a discord account")]
+        [Permission(MyPromoteLevel.None)]
+        public async void link() {
+            HttpResponseMessage response;
+            using (HttpClient clients = new HttpClient()) {
+                List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>
+                {
+                        new KeyValuePair<string, string>("steamid",Context.Player.SteamUserId.ToString()),
+                };
+                FormUrlEncodedContent content = new FormUrlEncodedContent(pairs);
+                response = await clients.PostAsync("http://sedb.uk/guid-manager.php", content);
+            }
+            string texts = await response.Content.ReadAsStringAsync();
+            utils utils = new utils();
+            Dictionary<string,string> kvp = utils.ParseQueryString(texts);
+            if (kvp["existance"] == "false") {
+                MyVisualScriptLogicProvider.OpenSteamOverlay($"https://steamcommunity.com/linkfilter/?url=http://sedb.uk/?guid={kvp["guid"]}&steamid={Context.Player.SteamUserId}", Context.Player.IdentityId);
+            }
+            else {
+                Context.Respond("Your SteamId has already been linked to discord, if this has not been authenticated by yourself... Please contact your admin");
+            }
+            
         }
 
         [Command("bridge-enable", "To enable SEDB if disabled")]
---
+++ SEDiscordBridge/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using System.Collections.Generic;
+using System.Linq;
+using Torch.API;
+using System.Net.Http;
+using System.Web;
+
+namespace SEDiscordBridge {
+
+    public class utils {
+        public static ITorchBase Torch { get; }
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static string GetSubstringByString(string from, string until, string wholestring) {
+            return wholestring.Substring((wholestring.IndexOf(from) + from.Length), (wholestring.IndexOf(until) - wholestring.IndexOf(from) - from.Length));
+        }
+
+        public static Dictionary<string, string> ParseQueryString(string queryString) {
+            var nvc = HttpUtility.ParseQueryString(queryString);
+            return nvc.AllKeys.ToDictionary(k => k, k => nvc[k]);
+        }
+    }
+}
