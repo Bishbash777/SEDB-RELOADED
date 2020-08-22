05efc08419090744591375fa622368a71154d435
Jack Bishop <Administrator@WIN-RV9T648P3OV>
2020-08-22 22:48:54 +0200

    Add overflow static method for passing in channel ID

--- SEDiscordBridge/DiscordBridge.cs
+++ SEDiscordBridge/DiscordBridge.cs
@@ -120,6 +120,10 @@ namespace SEDiscordBridge
             await Discord.SendMessageAsync(Discord.GetChannelAsync(ulong.Parse(Plugin.Config.ChatChannelId)).Result, message);
         }
 
+        public static async void SendDiscordMessageStatic(string message, string channelID) {
+            await Discord.SendMessageAsync(Discord.GetChannelAsync(ulong.Parse(channelID)).Result, message);
+        }
+
         public void SendSimMessage(string msg)
         {
             try
